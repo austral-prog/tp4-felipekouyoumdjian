0ea8c0a693f8ad94c4ee4b2e08dfa283e0ea8c31
felipekouyoumdjian <fkouyoumdjian@mail.austral.edu.ar>
2025-09-07 23:09:56 -0300

    Add files via upload

--- leap_year.py
+++ leap_year.py
@@ -1,2 +1,8 @@
 def leap_year():
-    print("TO DO")
+
+    YEAR = int(input("Ingrese un año: "))
+    
+    if YEAR % 4 == 0 and YEAR % 100 != 0 or YEAR % 400 == 0:
+        print(f"El año {YEAR} es bisiesto")
+    else:
+        print(f"El año {YEAR} no es bisiesto")
